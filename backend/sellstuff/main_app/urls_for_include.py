1dc2347e97ba6d26ed0d484892d150e4e6d9787b
GitMaksimHub <macdacin2345@gmail.com>
2024-06-02 14:51:44 +0300

    everything works

--- backend/sellstuff/main_app/urls_for_include.py
+++ backend/sellstuff/main_app/urls_for_include.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.main, name="main"),
-    path('test_path/<slug:slug_id>', views.test_path, name="test_path")
+    path('test_path/<slug:slug_id>', views.test_path, name="test_path"),
 ]
