c9f1a6d727ad80683613bae0d301c6cd1d66c197
johan <kkzemeow@gmail.com>
2024-06-03 20:16:54 +0300

    check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// checked
 function App() {
-    const [details, setDetails] = useState([]);
+  const [details, setDetails] = useState([]);
 
-    useEffect(() => {
-        axios.get('http://localhost:8000/test_api')
-            .then(res => {
-                setDetails(res.data);
-            })
-            .catch(err => {
-                console.error('Ошибка при получении данных:', err);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/test_api")
+      .then((res) => {
+        setDetails(res.data);
+      })
+      .catch((err) => {
+        console.error("Ошибка при получении данных:", err);
+      });
+  }, []);
 
-    return (
-        <div>
-            <header>Данные из Django</header>
-            <hr></hr>
-            <ul>
-                {details.map((item, index) => (
-                    <li key={index}>
-                        <h2>{item.title}</h2>
-                        <p>{item.description}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <header>Данные из Django</header>
+      <hr></hr>
+      <ul>
+        {details.map((item, index) => (
+          <li key={index}>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
-
-
